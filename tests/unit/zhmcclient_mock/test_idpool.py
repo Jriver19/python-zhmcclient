b09493672eb2322ad7fe9b5e8beba7a3b9feb526
Andreas Maier <maiera@de.ibm.com>
2020-08-14 15:59:24 +0200

    Third chunk of Pylint issue resolutions that were not errors
    Details:
    
    * In zhmcclient/_utils.py, fixed import of OrderedDict that was incorrectly
      attempted to be imported from from collections.abc. This avoids a
      DeprecationWarning.
    
    * In test_urihandler.py, combined multiple test functions to parametrized
      test functions test_parse_query_parms(), test_urihandler_handle_cpc().
    
    * Moved the test methods of the following test classes to become module-global,
      removing the test classes:
      - In test_urihandler.py: TestHTTPError, TestConnectionError,
        TestInvalidResourceError, TestInvalidMethodError, TestCpcNotInDpmError,
        TestCpcInDpmError, TestParseQueryParms, TestUriHandlerHandlerEmpty,
        TestUriHandlerHandlerSimple
      - In test_hmc.py: TestFakedMetricGroupDefinition, TestFakedMetricObjectValues
      - In test_idpool.py: TestIdPool
    
    * In test_urihandler.py, renamed several test functions to make them
      unique within the module (they were only unique within their classes)
      in order to make them individually invokable using pytest, and for
      more consistency.
    
    * Added missing docstrings to all classes, functions, methods, in various
      test modules.
    
    * Resolved various other Pylint issues that did not constitute real errors,
      without changng anything in the program logic.
    
    Signed-off-by: Andreas Maier <maiera@de.ibm.com>

--- tests/unit/zhmcclient_mock/test_idpool.py
+++ tests/unit/zhmcclient_mock/test_idpool.py
@@ -26,125 +26,156 @@ from zhmcclient_mock._idpool import IdPool
 urllib3.disable_warnings()
 
 
-class TestIdPool(object):
-    """All tests for class IdPool."""
+def test_init_error_1():
+    """
+    Test invalid IdPool.__init__() with lowest > highest.
+    """
 
-    def test_init_error_1(self):
+    with pytest.raises(ValueError):
+        IdPool(7, 6)
 
-        with pytest.raises(ValueError):
-            IdPool(7, 6)
 
-    def test_invalid_free_error_1(self):
+def test_invalid_free_error_1():
+    """
+    Test invalid IdPool.free() in some variations.
+    """
 
-        pool = IdPool(5, 5)
+    pool = IdPool(5, 5)
 
-        with pytest.raises(ValueError):
-            pool.free(4)  # not in range
+    with pytest.raises(ValueError):
+        pool.free(4)  # not in range
 
-        with pytest.raises(ValueError):
-            pool.free(5)  # in range but not allocated
+    with pytest.raises(ValueError):
+        pool.free(5)  # in range but not allocated
 
-        with pytest.raises(ValueError):
-            pool.free(6)  # not in range
+    with pytest.raises(ValueError):
+        pool.free(6)  # not in range
 
-    def test_invalid_free_error_2(self):
 
-        pool = IdPool(5, 5)
+def test_invalid_free_error_2():
+    """
+    Test invalid IdPool.free_if_allocated() in some variations.
+    """
 
-        pool.free_if_allocated(4)  # not in range (= not allocated)
+    pool = IdPool(5, 5)
 
-        pool.free_if_allocated(5)  # in range but not allocated
+    pool.free_if_allocated(4)  # not in range (= not allocated)
 
-        pool.free_if_allocated(6)  # not in range (= not allocated)
+    pool.free_if_allocated(5)  # in range but not allocated
 
-    def _test_exhausting_for_lo_hi(self, lowest, highest):
-        start = lowest
-        end = highest + 1
+    pool.free_if_allocated(6)  # not in range (= not allocated)
 
-        pool = IdPool(lowest, highest)
 
-        # Exhaust the pool
-        id_list = []
-        for i in range(start, end):
-            id = pool.alloc()
-            id_list.append(id)
+def _test_exhausting_for_lo_hi(lowest, highest):
+    """
+    Internal helper function that tests exhausting an IdPool.
+    """
 
-        # Verify uniqueness of the ID values
-        id_set = set(id_list)
-        assert len(id_set) == len(id_list)
+    start = lowest
+    end = highest + 1
 
-        # Verify that the pool is exhausted
-        with pytest.raises(ValueError):
-            pool.alloc()
+    pool = IdPool(lowest, highest)
 
-    def _test_free_for_lo_hi(self, lowest, highest):
-        start = lowest
-        end = highest + 1
+    # Exhaust the pool
+    id_list = []
+    for _ in range(start, end):
+        _id = pool.alloc()
+        id_list.append(_id)
 
-        pool = IdPool(lowest, highest)
+    # Verify uniqueness of the ID values
+    id_set = set(id_list)
+    assert len(id_set) == len(id_list)
 
-        # Exhaust the pool
-        id_list1 = []
-        for i in range(start, end):
-            id = pool.alloc()
-            id_list1.append(id)
+    # Verify that the pool is exhausted
+    with pytest.raises(ValueError):
+        pool.alloc()
 
-        # Return everything to the pool
-        for id in id_list1:
-            pool.free(id)
 
-        # Verify that nothing is used in the pool
-        assert len(pool._used) == 0
+def _test_free_for_lo_hi(lowest, highest):
+    """
+    Internal helper function that tests exhausting, freeing, and again
+    exhausting an IdPool.
+    """
 
-        # Exhaust the pool
-        id_list2 = []
-        for i in range(start, end):
-            id = pool.alloc()
-            id_list2.append(id)
+    start = lowest
+    end = highest + 1
 
-        # Verify that the same ID values came back as last time
-        assert set(id_list1) == set(id_list2)
+    pool = IdPool(lowest, highest)
 
-        # Verify that the pool is exhausted
-        with pytest.raises(ValueError):
-            pool.alloc()
+    # Exhaust the pool
+    id_list1 = []
+    for _ in range(start, end):
+        _id = pool.alloc()
+        id_list1.append(_id)
 
-    def _test_all_for_lo_hi(self, lowest, highest):
-        self._test_exhausting_for_lo_hi(lowest, highest)
-        self._test_free_for_lo_hi(lowest, highest)
+    # Return everything to the pool
+    for _id in id_list1:
+        pool.free(_id)
 
-    def test_all(self):
-        # Knowing that the chunk size is 10, we focus on the sizes and range
-        # boundaries around that
-        self._test_all_for_lo_hi(0, 0)
-        self._test_all_for_lo_hi(0, 1)
-        self._test_all_for_lo_hi(0, 9)
-        self._test_all_for_lo_hi(0, 10)
-        self._test_all_for_lo_hi(0, 11)
-        self._test_all_for_lo_hi(0, 19)
-        self._test_all_for_lo_hi(0, 20)
-        self._test_all_for_lo_hi(0, 21)
-        self._test_all_for_lo_hi(3, 3)
-        self._test_all_for_lo_hi(3, 4)
-        self._test_all_for_lo_hi(3, 9)
-        self._test_all_for_lo_hi(3, 10)
-        self._test_all_for_lo_hi(3, 11)
-        self._test_all_for_lo_hi(3, 12)
-        self._test_all_for_lo_hi(3, 13)
-        self._test_all_for_lo_hi(3, 14)
-        self._test_all_for_lo_hi(9, 9)
-        self._test_all_for_lo_hi(9, 10)
-        self._test_all_for_lo_hi(9, 11)
-        self._test_all_for_lo_hi(9, 18)
-        self._test_all_for_lo_hi(9, 19)
-        self._test_all_for_lo_hi(9, 20)
-        self._test_all_for_lo_hi(10, 10)
-        self._test_all_for_lo_hi(10, 11)
-        self._test_all_for_lo_hi(10, 19)
-        self._test_all_for_lo_hi(10, 20)
-        self._test_all_for_lo_hi(10, 21)
-        self._test_all_for_lo_hi(11, 11)
-        self._test_all_for_lo_hi(11, 12)
-        self._test_all_for_lo_hi(11, 20)
-        self._test_all_for_lo_hi(11, 21)
-        self._test_all_for_lo_hi(11, 22)
+    # Verify that nothing is used in the pool
+    assert len(pool._used) == 0  # pylint: disable=protected-access
+
+    # Exhaust the pool
+    id_list2 = []
+    for _ in range(start, end):
+        _id = pool.alloc()
+        id_list2.append(_id)
+
+    # Verify that the same ID values came back as last time
+    assert set(id_list1) == set(id_list2)
+
+    # Verify that the pool is exhausted
+    with pytest.raises(ValueError):
+        pool.alloc()
+
+
+def _test_all_for_lo_hi(lowest, highest):
+    """
+    Internal helper function that just combines _test_exhausting_for_lo_hi()
+    and _test_free_for_lo_hi().
+    """
+    # TODO: Convert to use fixtures or parametrization
+    _test_exhausting_for_lo_hi(lowest, highest)
+    _test_free_for_lo_hi(lowest, highest)
+
+
+def test_all():
+    """
+    Test pool exhaustion, etc. for some pool sizes.
+
+    Knowing that the chunk size is 10, we focus on the sizes and range
+    boundaries around that
+    """
+    # TODO: Convert to use fixtures or parametrization
+    _test_all_for_lo_hi(0, 0)
+    _test_all_for_lo_hi(0, 1)
+    _test_all_for_lo_hi(0, 9)
+    _test_all_for_lo_hi(0, 10)
+    _test_all_for_lo_hi(0, 11)
+    _test_all_for_lo_hi(0, 19)
+    _test_all_for_lo_hi(0, 20)
+    _test_all_for_lo_hi(0, 21)
+    _test_all_for_lo_hi(3, 3)
+    _test_all_for_lo_hi(3, 4)
+    _test_all_for_lo_hi(3, 9)
+    _test_all_for_lo_hi(3, 10)
+    _test_all_for_lo_hi(3, 11)
+    _test_all_for_lo_hi(3, 12)
+    _test_all_for_lo_hi(3, 13)
+    _test_all_for_lo_hi(3, 14)
+    _test_all_for_lo_hi(9, 9)
+    _test_all_for_lo_hi(9, 10)
+    _test_all_for_lo_hi(9, 11)
+    _test_all_for_lo_hi(9, 18)
+    _test_all_for_lo_hi(9, 19)
+    _test_all_for_lo_hi(9, 20)
+    _test_all_for_lo_hi(10, 10)
+    _test_all_for_lo_hi(10, 11)
+    _test_all_for_lo_hi(10, 19)
+    _test_all_for_lo_hi(10, 20)
+    _test_all_for_lo_hi(10, 21)
+    _test_all_for_lo_hi(11, 11)
+    _test_all_for_lo_hi(11, 12)
+    _test_all_for_lo_hi(11, 20)
+    _test_all_for_lo_hi(11, 21)
+    _test_all_for_lo_hi(11, 22)
